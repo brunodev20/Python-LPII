f6ca23e8e9dc32c5bf413b09c9299b6e2713ce0c
brunodev20 <61809926+brunodev20@users.noreply.github.com>
2020-08-26 20:42:25 -0300

    Delete 9.py

--- 9.py
+++
@@ -1,9 +0,0 @@
-"""
-9 - Faça um método que receba um número X e uma palavra no console e repita x
-vezes a essa frase.
-"""
-
-x=int(input("Digite um número. Este número será o número de vezes que a palavra que você digitar se repetirá: "))
-palavra=input("Digite uma palavra: ")
-
-print((palavra+"\n")*x)
