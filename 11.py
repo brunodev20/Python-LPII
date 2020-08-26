d1948f1c51c2584e038e20c0d0b175cf6698c9aa
brunodev20 <61809926+brunodev20@users.noreply.github.com>
2020-08-26 20:41:05 -0300

    Delete 11.py

--- 11.py
+++
@@ -1,8 +0,0 @@
-"""
-11 - Escreva um algoritmo para ler um valor (do teclado) e escrever (na tela) o seu
-antecessor.
-"""
-
-valor=int(input("Digite um número: "))
-
-print("O antecessor deste valor é ",valor-1)
